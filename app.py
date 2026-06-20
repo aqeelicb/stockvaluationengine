484a3010d35427f0bc04d2915c24fd352129f09f
aqeelicb <aqeel.icb@gmail.com>
2026-06-20 19:23:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import plotly.graph_objects as go
 ############################################################
 
 st.set_page_config(
-    page_title="Stock Valuation App by Aqeel",
+    page_title="Stock Valuation Engine by Aqeel",
     layout="wide"
 )
 
